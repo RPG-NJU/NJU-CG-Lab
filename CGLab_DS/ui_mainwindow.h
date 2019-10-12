2526c2fb67dcb60a542ec88ab7f868b897314f9a
HELLORPG <hellorpg2017@gmail.com>
2019-10-12 11:24:35 +0800

    [add] .gitignore中增加了vs编译之后新增的x64目录

--- CGLab_DS/ui_mainwindow.h
+++ CGLab_DS/ui_mainwindow.h
@@ -10,10 +10,8 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
@@ -24,11 +22,8 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QMenuBar *menubar;
-    QMenu *menu;
-    QMenu *menu_2;
-    QMenu *menuabout;
     QStatusBar *statusbar;
+    QMenuBar *menubar;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -38,23 +33,13 @@ public:
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         MainWindow->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(MainWindow);
-        menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 25));
-        menu = new QMenu(menubar);
-        menu->setObjectName(QString::fromUtf8("menu"));
-        menu_2 = new QMenu(menubar);
-        menu_2->setObjectName(QString::fromUtf8("menu_2"));
-        menuabout = new QMenu(menubar);
-        menuabout->setObjectName(QString::fromUtf8("menuabout"));
-        MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
-
-        menubar->addAction(menu->menuAction());
-        menubar->addAction(menu_2->menuAction());
-        menubar->addAction(menuabout->menuAction());
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 25));
+        MainWindow->setMenuBar(menubar);
 
         retranslateUi(MainWindow);
 
@@ -64,9 +49,6 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        menu->setTitle(QCoreApplication::translate("MainWindow", "\345\205\263\344\272\216", nullptr));
-        menu_2->setTitle(QCoreApplication::translate("MainWindow", "\346\200\216\344\271\210", nullptr));
-        menuabout->setTitle(QCoreApplication::translate("MainWindow", "about", nullptr));
     } // retranslateUi
 
 };
