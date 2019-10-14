7f0846bde76345ca023206d125158b1c9c029ff3
HELLORPG <hellorpg2017@gmail.com>
2019-10-14 20:19:49 +0800

    [fix] 将NULL替换为nullptr更加符合C++的规范

--- CGLab_DS/AboutAuthorDialog.cpp
+++ CGLab_DS/AboutAuthorDialog.cpp
@@ -23,13 +23,13 @@ void AboutAuthorDialog::on_buttonAuthorGithub_clicked()
 	// 这种实现方法并不确定在除了Windows10之外的系统上使用的正确性
 	//wchar_t method[5] = "open";
 	// 参数形式是wchar_t形式，所以需要使用 L 来解释
-	ShellExecute(NULL, L"open", L"https://github.com/HELLORPG", NULL, NULL, SW_SHOWMAXIMIZED);
+	ShellExecute(nullptr, L"open", L"https://github.com/HELLORPG", nullptr, nullptr, SW_SHOWMAXIMIZED);
 	// 使用system的方法，会在打开之前产生一个命令行窗口，而使用ShellExecute就不会，不过会有系统兼容性问题
 	return;
 }
 
 void AboutAuthorDialog::on_buttonConnectAuthor_clicked()
 {
-	ShellExecute(NULL, L"open", L"mailto:officialrpg2017@gmail.com", NULL, NULL, SW_SHOWNORMAL);
+	ShellExecute(nullptr, L"open", L"mailto:officialrpg2017@gmail.com", nullptr, nullptr, SW_SHOWNORMAL);
 	return;
 }
