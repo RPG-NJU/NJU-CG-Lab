95a928c15bce66776ea5b23c526a8fcabfd4b441
HELLORPG <hellorpg2017@gmail.com>
2019-10-29 21:44:39 +0800

    [del] 删除了无用的曾用代码注释

--- CGLab_DS/DrawingArea_File.cpp
+++ CGLab_DS/DrawingArea_File.cpp
@@ -4,25 +4,6 @@
 // 文件部分
 bool DrawingArea::openCommandFile(QString file_path)
 {
-	//QFile file(file_path);
-	//if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-	//	QMessageBox::warning(this, tr("Read File"),
-	//		tr("Cannot open file:\n%1").arg(file_path));
-	//	return;
-	//}
-	//QTextStream in(&file);
-	//
-	//qDebug() << in.readLine() << endl;
-	////QTextEdit *textEdit = new QTextEdit(this);
-	//
-	////show_command_text->setText(in.readAll());
-	////while (!in.atEnd())
-	////{
-	//QTextEdit* show_command_text = new QTextEdit(this);
-	//	show_command_text->append(in.readLine());
-	////}
-	//qDebug() << "new edit" << endl;
-	//file.close();
 	QFile command_file(file_path);
 	if (!command_file.open(QIODevice::ReadOnly | QIODevice::Text)) // 如果文件打开失败
 	{
