ebca352f86ed1eaf723a5fa366a138ea583151e8
HELLORPG <hellorpg2017@gmail.com>
2019-10-31 00:29:49 +0800

    [add] 增加了过滤git中bmp文件的规则，并且支持到了直线的命令文件

--- CGLab_DS/DrawingArea_Command.cpp
+++ CGLab_DS/DrawingArea_Command.cpp
@@ -1,5 +1,7 @@
 #include "DrawingArea.h"
 
+using std::atoi;
+
 void DrawingArea::runCommand()
 {
 	for (const auto &command : commands) // 对每一条指令进行解析
@@ -8,7 +10,7 @@ void DrawingArea::runCommand()
 			continue;
 		if (command[0] == "resetCanvas") // 设置画布大小
 		{
-			int width(std::stoi(command[1])), height(std::stoi(command[2]));
+			const int width(std::stoi(command[1])), height(std::stoi(command[2]));
 			setPaperSize(width, height);
 		}
 
@@ -24,7 +26,24 @@ void DrawingArea::runCommand()
 			QColor color(c);
 			setPenColor(color);
 		}
-		
+
+		else if (command[0] == "drawLine") // 画直线
+		{
+			const int id(stoi(command[1])), x1(stoi(command[2])), y1(stoi(command[3])), x2(stoi(command[4])), y2(stoi(command[5]));
+			
+			vector<MyPoint> points;
+			QPainter painter(&paper); // 使用QImage初始化QPainter，需要使用指针
+			painter.setPen(pen);
+			
+			points = command[6] == "DDA" ? createStraightLineByDDA(x1, y1, x2, y2) : createStraightLineByBresenham(x1, y1, x2, y2);
+
+			for (const auto &point : points)
+			{
+				painter.drawPoint(point.x, point.y);
+			}
+
+			this->update();
+		}
 	}
 	
 	return;
